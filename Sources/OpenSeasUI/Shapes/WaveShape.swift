2945c005b94945a5b787f29d88be5151dcc44be4
Florian-Rh <florian@rhein.me>
2025-04-08 15:02:54 +0200

    Restructure shape and make it internal

--- Sources/OpenSeasUI/Shapes/WaveShape.swift
+++ Sources/OpenSeasUI/Shapes/WaveShape.swift
@@ -7,61 +7,76 @@
 
 import SwiftUI
 
-public struct WaveShape: Shape {
+internal struct WaveShape: Shape {
     private let amplitude: CGFloat
     private let waveLength: CGFloat
     private let waterLevel: CGFloat
     private(set) var phase: CGFloat = 0.0
     private let rotation: CGFloat
 
-    public init(
+    internal init(
         amplitude: CGFloat,
         waveLength: CGFloat,
-        waterLevel: CGFloat,
-        phase: CGFloat = 0.0 * .pi,
+        waterLevel: CGFloat = 0.5,
+        phase: CGFloat = 0.0,
         rotation: CGFloat = 0.0
     ) {
         self.amplitude = amplitude
-        self.waveLength = waveLength
+
+        // Wave length is a divisor in the sine function and must be bigger then zero
+        self.waveLength = max(waveLength, CGFloat.leastNormalMagnitude)
         self.waterLevel = waterLevel
         self.phase = phase
-        self.rotation = rotation
+
+        // Rotation is only defined between -.pi/4 and .pi/4 (or -45° and 45°).
+        self.rotation = min(max(rotation, -(.pi / 4)), .pi / 4)
     }
 
-    public var animatableData: Double {
+    internal var animatableData: Double {
         get { phase }
         set { phase = newValue }
     }
 
-    public func path(in rect: CGRect) -> Path {
+    internal func path(in rect: CGRect) -> Path {
         var path = Path()
-        let midHeight = rect.height / 2
         let width = rect.width
 
         // Calculate how much longer the waves need to be in order to fill the entire rectangle, if the surface line is rotated
-        let baseLength = width / 2
-        let hypothenuse = baseLength / cos(rotation)
-        let rotationElongation = (hypothenuse - baseLength) + amplitude * 2
+        let hypothenuse = width / cos(rotation)
+        let rotationElongation = (hypothenuse - width) + amplitude
         let startX: CGFloat = 0.0 - rotationElongation
         let endX: CGFloat = width + rotationElongation
 
-        path.move(to: CGPoint(x: startX, y: midHeight))
+        // Draw the sine curve
+        path.move(to: CGPoint(x: startX, y: 0.0))
         for xPoint in stride(from: startX, to: endX, by: 1) {
             let relativeX = xPoint / width
             let sine = sin(relativeX * (1 / waveLength) * .pi + phase)
-            let yPoint = midHeight + amplitude * sine
+            let yPoint = amplitude * sine
             path.addLine(to: CGPoint(x: xPoint, y: yPoint))
         }
 
-        path = path.rotation(.radians(rotation)).path(in: rect)
-        let verticalTranslation = rect.height * (1 - waterLevel) - midHeight
-        path = path.transform(.init(translationX: 0, y: verticalTranslation)).path(in: rect)
+        // Rotate the top water line
+        path = path
+            .rotation(.radians(rotation), anchor: .top)
+            .path(in: rect)
+
+        // Translate to the desired water level
+        let verticalTranslation = rect.height * (1 - waterLevel)
+        path = path
+            .transform(.init(translationX: 0, y: verticalTranslation))
+            .path(in: rect)
+
+        // Fill the area under the water line
         path.addLine(to: CGPoint(x: width, y: rect.height))
         path.addLine(to: CGPoint(x: 0, y: rect.height))
         path.closeSubpath()
-        let box = Path(roundedRect: rect, cornerRadius: 0.0)
 
-        return path.intersection(box)
+        // Truncate anything outside the bounding rectangle
+        let box = Path(roundedRect: rect, cornerRadius: 0.0)
+        path = path.intersection(box)
+
+        return path
     }
 }
 
@@ -71,8 +86,10 @@ public struct WaveShape: Shape {
         amplitude: 10,
         waveLength: 0.1,
         waterLevel: 0.5,
-        phase: 0.0,
+        phase: 2 * .pi,
         rotation: 0.0
     )
+    .frame(width: 250, height: 250)
+    .border(.red)
     .ignoresSafeArea()
 }
