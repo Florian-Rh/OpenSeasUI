18dfdc3379a45790e79b8ce77f1d213d1831a535
Florian-Rh <florian@rhein.me>
2025-07-26 11:25:27 +0200

    Add color demo view

---
+++ Sources/OpenSeasUI/Demo/ColorDemoView.swift
@@ -0,0 +1,53 @@
+//
+//  SwiftUIView.swift
+//  OpenSeasUI
+//
+//  Created by Florian Rhein on 04.07.25.
+//
+
+import SwiftUI
+
+#Preview("midnightAbyssGradient") {
+    LinearGradient(gradient: .midnightAbyssGradient, startPoint: .top, endPoint: .bottom)
+}
+
+#Preview("Colors") {
+    List {
+        ZStack {
+            Color.waveBlue
+            Text("waveBlue").foregroundStyle(.white)
+        }
+        ZStack {
+            Color.oceanBlue
+            Text("oceanBlue").foregroundStyle(.white)
+        }
+        ZStack {
+            Color.deepSeaBlue
+            Text("deepSeaBlue").foregroundStyle(.white)
+        }
+        ZStack {
+            Color.abyssBlue
+            Text("abyssBlue").foregroundStyle(.white)
+        }
+        ZStack {
+            Color.iceBlue
+            Text("iceBlue").foregroundStyle(.white)
+        }
+        ZStack {
+            Color.coralRed
+            Text("coralRed").foregroundStyle(.white)
+        }
+        ZStack {
+            Color.sunsetOrange
+            Text("sunsetOrange").foregroundStyle(.white)
+        }
+        ZStack {
+            Color.seafoamGreen
+            Text("seafoamGreen").foregroundStyle(.white)
+        }
+        ZStack {
+            Color.sandBeige
+            Text("sandBeige").foregroundStyle(.white)
+        }
+    }
+}
