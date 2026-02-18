dd9f8c081e13efa913968c27aeb70aaa05267659
Florian Rhein <florian@rhein.me>
2026-02-18 08:00:54 +0100

    Making SeededRandomNumberGenerator public

--- Sources/OpenSeasUI/SeededRandomNumberGenerator.swift
+++ Sources/OpenSeasUI/SeededRandomNumberGenerator.swift
@@ -7,13 +7,12 @@
 
 import Foundation
 
-class SeededRandomNumberGenerator: RandomNumberGenerator {
-
-    init(seed: Int) {
+public class SeededRandomNumberGenerator: RandomNumberGenerator {
+    public init(seed: Int) {
         srand48(seed)
     }
 
-    func next() -> UInt64 {
+    public func next() -> UInt64 {
         return UInt64(drand48() * Double(UInt64.max))
     }
 }
